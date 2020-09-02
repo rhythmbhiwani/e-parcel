d85d8cf84574fa88c4303c35ae2bbf2f52060040
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-09-03 01:14:00 +0530

    Package Type Selection Modal Done

--- lib/screens/main/components/book.dart
+++ lib/screens/main/components/book.dart
@@ -1,3 +1,4 @@
+import 'package:E_Parcel/components/default_button.dart';
 import 'package:E_Parcel/screens/address/enter_address_screen.dart';
 import 'package:E_Parcel/screens/main/components/mini_components/dash.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -112,7 +113,7 @@ class Book extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20),
-                  buildPackagingSelectionArea()
+                  buildPackagingSelectionArea(context)
                 ],
               );
             },
@@ -122,7 +123,7 @@ class Book extends StatelessWidget {
     );
   }
 
-  Container buildPackagingSelectionArea() {
+  Container buildPackagingSelectionArea(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8.0),
@@ -149,13 +150,13 @@ class Book extends StatelessWidget {
           ),
           Container(
             width: SizeConfig.screenWidth * 0.80,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
                     "PACKAGE TYPE",
                     style: TextStyle(
                       fontSize: 18,
@@ -163,21 +164,13 @@ class Book extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  DropdownButton(
-                    isExpanded: true,
-                    items: [
-                      DropdownMenuItem(child: Text("Food")),
-                      DropdownMenuItem(child: Text("Medicians")),
-                      DropdownMenuItem(child: Text("Groceries")),
-                      DropdownMenuItem(child: Text("Documents")),
-                      DropdownMenuItem(child: Text("Electronics")),
-                      DropdownMenuItem(child: Text("Cloths")),
-                      DropdownMenuItem(child: Text("Other")),
-                    ],
-                    onChanged: (value) {},
-                  ),
-                ],
-              ),
+                ),
+                Theme(
+                    data: Theme.of(context).copyWith(
+                      canvasColor: Colors.transparent,
+                    ),
+                    child: PackageTypeSelector()),
+              ],
             ),
           )
         ],
@@ -217,5 +210,157 @@ class Book extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class PackageTypeSelector extends StatelessWidget {
+  const PackageTypeSelector({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _showModalBottomSheet(context) {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            height: 500,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(100),
+              ),
+            ),
+            child: ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 20,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Select Category",
+                                style: TextStyle(
+                                  fontSize: getProportionateScreenWidth(20),
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Text("Select the items to send"),
+                            ],
+                          ),
+                          SvgPicture.asset(
+                            "assets/icons/delivery-list.svg",
+                            height: 50,
+                          ),
+                        ],
+                      ),
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.fastfood),
+                      title: Text("Food"),
+                      value: false,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.accessibility_new),
+                      title: Text("Clothes"),
+                      value: true,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.business_center),
+                      title: Text("Documents"),
+                      value: false,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.shopping_basket),
+                      title: Text("Groceries"),
+                      value: true,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.accessible),
+                      title: Text("Medicines"),
+                      value: false,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.laptop_mac),
+                      title: Text("Electronics"),
+                      value: true,
+                      onChanged: null,
+                    ),
+                    CheckboxListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 60),
+                      secondary: Icon(Icons.assignment),
+                      title: Text("Others"),
+                      value: false,
+                      onChanged: null,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 60, vertical: 20),
+                      child: DefaultButton(
+                        text: "Save & Continue",
+                        press: () {},
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          );
+        },
+      );
+    }
+
+    return FlatButton(
+        onPressed: () {
+          _showModalBottomSheet(context);
+        },
+        child: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.black12,
+                width: 3.0,
+              ),
+            ),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text("Select Category"),
+                flex: 5,
+              ),
+              Expanded(
+                flex: 1,
+                child: Icon(Icons.arrow_drop_down_circle),
+              ),
+            ],
+          ),
+        ));
   }
 }
